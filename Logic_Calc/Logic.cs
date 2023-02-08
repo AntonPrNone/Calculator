da373f869607861dd291fa2b884daeb32a7be97a
Антон Ахтямов <yznikfortyni@gmail.com>
2023-02-08 11:07:45 +0300

    Работа с операторами

--- Logic_Calc/Logic.cs
+++ Logic_Calc/Logic.cs
@@ -7,162 +7,182 @@ using System.Windows.Input;
 
 namespace Logic_Calc
 {
-    public class Logic
-    {
-        public enum Corner { Degrees, Radians, Grads };
-        public Corner СornerType = Corner.Degrees;
-        public double Angle;
-        public bool ReplaceNextTimeClick;
-        public StringBuilder Expression = new StringBuilder("");
-        public StringBuilder LastNumber = new StringBuilder("");
-        public char[] OperatorPriorities;
-        public char[] Operators = { '+', '-', '*', '/', '^', '!', '%' };
-        public void ProcessingKey(double figure) // Перегрузка числа
-        {
-            if (ReplaceNextTimeClick)
-            {
-                LastNumber.Clear();
-                ReplaceNextTimeClick = false;
-            }
+	public class Logic
+	{
+		public enum Corner { Degrees, Radians, Grads };
+		public Corner СornerType = Corner.Degrees;
+		public double Angle;
+		public bool ReplaceNextTimeClick;
+		public StringBuilder Expression = new StringBuilder("");
+		public StringBuilder LastNumber = new StringBuilder("");
+		public char[] OperatorPriorities;
+		public char[] Operators = { '+', '-', '*', '/', '^', '!', '%' };
+		public void ProcessingKey(double figure) // Перегрузка числа
+		{
+			if (ReplaceNextTimeClick)
+			{
+				LastNumber.Clear();
+				ReplaceNextTimeClick = false;
+			}
 
-            Expression.Append(figure);
-            LastNumber.Append(figure);
-        }
+			Expression.Append(figure);
+			LastNumber.Append(figure);
+		}
 
-        public void ProcessingKey(char chr) // Перегрузка разделителя дробной части числа
-        {
-            if (chr == ',')
-            {
-                Expression.Append(chr);
-                LastNumber.Append(chr);
-            } 
-        }
-        public void ProcessingKey(string text) // Перегрузка операторов
-        {
-            if (ReplaceNextTimeClick)
-            {
-                LastNumber.Clear();
-                ReplaceNextTimeClick = false;
-            }
+		public void ProcessingKey(char chr) // Перегрузка разделителя дробной части числа
+		{
+			if (chr == ',')
+			{
+				Expression.Append(chr);
+				LastNumber.Append(chr);
+			} 
+		}
+		public void ProcessingKey(string text) // Перегрузка операторов
+		{
+			if (ReplaceNextTimeClick)
+			{
+				LastNumber.Clear();
+				ReplaceNextTimeClick = false;
+			}
 
-            switch (text)
-            {
-                case "⟵":
-                    if (LastNumber.Length != 0)
-                    {
-                        LastNumber.Remove(LastNumber.Length - 1, 1);
-                        Expression.Remove(Expression.Length - 1, 1);
-                    }
-                    break;
+			switch (text)
+			{
+				case "⟵":
+					if (LastNumber.Length != 0)
+					{
+						LastNumber.Remove(LastNumber.Length - 1, 1);
+						Expression.Remove(Expression.Length - 1, 1);
+					}
+					break;
 
-                case "C":
-                    LastNumber.Remove(LastNumber.Length - 1, 1);
-                    Expression.Remove(Expression.Length - 1, 1);
-                    break;
+				case "C":
+					LastNumber.Remove(LastNumber.Length - 1, 1);
+					Expression.Remove(Expression.Length - 1, 1);
+					break;
 
-                case "CE":
-                    Expression.Clear();
-                    LastNumber.Clear();
-                    ReplaceNextTimeClick = true;
-                    break;
+				case "CE":
+					Expression.Clear();
+					LastNumber.Clear();
+					ReplaceNextTimeClick = true;
+					break;
 
-                case "±":
-                    LastNumber = new StringBuilder(Int32.Parse(LastNumber.ToString()) * -1);
-                    Expression.Insert(Expression.Length - LastNumber.Length, "-");
-                    break;
+				case "±":
+					LastNumber = new StringBuilder(Int32.Parse(LastNumber.ToString()) * -1);
+					Expression.Insert(Expression.Length - LastNumber.Length, "-");
+					break;
 
-                case "+":
-                    Expression.Append(LastNumber);
-                    Expression.Append("+");
-                    ReplaceNextTimeClick = true;
-                    break;
+				case "+":
+					Expression.Append("+");
+					ReplaceNextTimeClick = true;
+					break;
 
-                case "-":
-                    Expression.Append(LastNumber);
-                    Expression.Append("-");
-                    ReplaceNextTimeClick = true;
-                    break;
+				case "-":
+					Expression.Append("-");
+					ReplaceNextTimeClick = true;
+					break;
 
-                case "*":
-                    Expression.Append(LastNumber);
-                    Expression.Append("*");
-                    ReplaceNextTimeClick = true;
-                    break;
+				case "*":
+					Expression.Append("*");
+					ReplaceNextTimeClick = true;
+					break;
 
-                case "/":
-                    Expression.Append(LastNumber);
-                    Expression.Append("/");
-                    ReplaceNextTimeClick = true;
-                    break;
+				case "/":
+					Expression.Append("/");
+					ReplaceNextTimeClick = true;
+					break;
 
-                // ---------------------------------
+				// ---------------------------------
 
-                case "sin":
-                    Angle = ConvertingAngle(Double.Parse(LastNumber.ToString()), СornerType);
-                    Expression.Append($"sin({Angle})");
-                    LastNumber = new StringBuilder(Math.Sin(Angle).ToString());
-                    break;
+				case "sin":
+					Angle = ConvertingAngle(Double.Parse(LastNumber.ToString()), СornerType);
+					Expression.Remove(Expression.Length - LastNumber.Length, LastNumber.Length);
+					Expression.Append(Math.Sin(Angle));
+					LastNumber = new StringBuilder(Math.Sin(Angle).ToString());
+					break;
 
-                case "cos":
-                    Angle = ConvertingAngle(Double.Parse(LastNumber.ToString()), СornerType);
-                    Expression.Append($"cos({Angle})");
-                    LastNumber = new StringBuilder(Math.Cos(Angle).ToString());
-                    break;
+				case "cos":
+					Angle = ConvertingAngle(Double.Parse(LastNumber.ToString()), СornerType);
+					Expression.Remove(Expression.Length - LastNumber.Length, LastNumber.Length);
+					Expression.Append(Math.Cos(Angle));
+					LastNumber = new StringBuilder(Math.Cos(Angle).ToString());
+					break;
 
-                case "tan":
-                    Angle = ConvertingAngle(Double.Parse(LastNumber.ToString()), СornerType);
-                    Expression.Append($"tan({Angle})");
-                    LastNumber = new StringBuilder(Math.Tan(Angle).ToString());
-                    break;
+				case "tan":
+					Angle = ConvertingAngle(Double.Parse(LastNumber.ToString()), СornerType);
+					Expression.Remove(Expression.Length - LastNumber.Length, LastNumber.Length);
+					Expression.Append(Math.Tan(Angle));
+					LastNumber = new StringBuilder(Math.Tan(Angle).ToString());
+					break;
 
-                case "sinh":
-                    Angle = ConvertingAngle(Double.Parse(LastNumber.ToString()), СornerType);
-                    Expression.Append($"sin({Angle})");
-                    LastNumber = new StringBuilder(Math.Sinh(Angle).ToString());
-                    break;
+				case "sinh":
+					Angle = ConvertingAngle(Double.Parse(LastNumber.ToString()), СornerType);
+					Expression.Remove(Expression.Length - LastNumber.Length, LastNumber.Length);
+					Expression.Append(Math.Sinh(Angle));
+					LastNumber = new StringBuilder(Math.Sinh(Angle).ToString());
+					break;
 
-                case "cosh":
-                    Angle = ConvertingAngle(Double.Parse(LastNumber.ToString()), СornerType);
-                    Expression.Append($"sin({Angle})");
-                    LastNumber = new StringBuilder(Math.Cosh(Angle).ToString());
-                    break;
+				case "cosh":
+					Angle = ConvertingAngle(Double.Parse(LastNumber.ToString()), СornerType);
+					Expression.Remove(Expression.Length - LastNumber.Length, LastNumber.Length);
+					Expression.Append(Math.Cosh(Angle));
+					LastNumber = new StringBuilder(Math.Cosh(Angle).ToString());
+					break;
 
-                case "tanh":
-                    Angle = ConvertingAngle(Double.Parse(LastNumber.ToString()), СornerType);
-                    Expression.Append($"sin({Angle})");
-                    LastNumber = new StringBuilder(Math.Tanh(Angle).ToString());
-                    break;
+				case "tanh":
+					Angle = ConvertingAngle(Double.Parse(LastNumber.ToString()), СornerType);
+					Expression.Remove(Expression.Length - LastNumber.Length, LastNumber.Length);
+					Expression.Append(Math.Tanh(Angle));
+					LastNumber = new StringBuilder(Math.Tanh(Angle).ToString());
+					break;
 
-                default:
-                    Expression.Append(text);
-                    break;
-            }
-        }
+				// ---------------------------------
 
-        public void CalculatingExpression()
-        {
-            foreach (var item in Expression.ToString())
-            { 
-                
-            }
-        }
+				case "!":
+					double LNumber = Double.Parse(LastNumber.ToString());
+					if (LNumber % 1 == 0) Factorial((int)LNumber);
+					else
+					{ 
+						LastNumber = new StringBuilder(Math.Log(Factorial((int)(LNumber))) + (LNumber - (int)(LNumber)) * Math.Log((int)LNumber + 1));
+					} 
+						break;
 
-        public static double ConvertingAngle(double angle, Corner cornerType)
-        {
-            switch (cornerType)
-            {
-                case Corner.Degrees:
-                    return angle;
+				//default:
+				//    Expression.Append(text);
+				//    break;
+			}
+		}
 
-                case Corner.Radians:
-                    return (angle * 180) / Math.PI;
+		public void CalculatingExpression()
+		{
+			foreach (var item in Expression.ToString())
+			{ 
+				
+			}
+		}
 
-                case Corner.Grads:
-                    return angle * 0.9;
+		public static double ConvertingAngle(double angle, Corner cornerType)
+		{
+			switch (cornerType)
+			{
+				case Corner.Degrees:
+					return angle * (Math.PI / 180);
 
-                default:
-                    return angle;
-            }
-        }
-    }
+				case Corner.Radians:
+					return angle;
+
+				case Corner.Grads:
+					return (angle * (Math.PI / 180)) * 0.9;
+
+				default:
+					return angle;
+			}
+		}
+
+		public static int Factorial(int number)
+		{
+			if (number == 1 || number == 0) return 1;
+
+			return number * Factorial(number - 1);
+		}
+	}
 }
